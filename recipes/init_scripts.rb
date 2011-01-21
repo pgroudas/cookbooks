1a2d38df0bf3ca77bb7da3675fb8c6d3dc601511
PJ <pgroudas@gmail.com>
2011-01-20 22:04:43 -0500

    finished init_scripts recipe

--- recipes/init_scripts.rb
+++ recipes/init_scripts.rb
@@ -17,3 +17,18 @@ cookbook_file "/etc/init.d/git-poller" do
   source "git-poller"
   mode 0755
 end
+
+cookbook_file "/etc/logrotate.d/gitorious" do
+  source "gitorious-logrotate"
+  mode 0755
+end
+
+bash "initialize-scripts" do
+  cwd "/tmp"
+  code <<-EOH
+    update-rc.d stomp defaults
+    update-rc.d git-daemon defaults
+    update-rc.d git-ultrasphinx defaults
+    update-rc.d git-poller defaults
+  EOH
+end
