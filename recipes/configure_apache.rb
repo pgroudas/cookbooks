06d5ec5153cbc1158d509f91095d0dad41a611a0
PJ <pgroudas@gmail.com>
2011-01-20 23:59:10 -0500

    finished apache config

--- recipes/configure_apache.rb
+++ recipes/configure_apache.rb
@@ -6,12 +6,34 @@ cookbook_file "/etc/apache2/mods-available/passenger.load" do
   source "passenger.load"
 end
 
+
 bash "enable-stuff" do
   code <<-EOH
     a2enmod passenger
     a2enmod rewrite
     a2enmod ssl
-    a2ensite default-ssl
+  EOH
+end
+
+cookbook_file "/etc/apache2/sites-available/gitorious" do
+  source "sites-available-gitorious"
+end
+
+cookbook_file "/etc/apache2/sites-available/gitorious-ssl" do
+  source "sites-available-gitorious-ssl"
+end
+
+bash "desable-sites" do
+  code <<-EOH
+    a2dissite default-ssl
+    a2dissite default
+    a2dissite gitorious-ssl
+  EOH
+end
+
+bash "enable-sites" do
+  code <<-EOH
+    a2ensite gitorious
   EOH
 end
 
