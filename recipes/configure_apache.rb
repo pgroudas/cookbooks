f05cef729d0a3233009f16dab74707014b076cd2
PJ <pgroudas@gmail.com>
2011-01-20 23:53:27 -0500

    apache sites configuration

--- recipes/configure_apache.rb
+++ recipes/configure_apache.rb
@@ -1,9 +1,21 @@
-bash "install-passenger-apache2-module" do
-  cwd "/"
-  code "/usr/bin/passenger-install-apache2-module -a"
-end
+#bash "install-passenger-apache2-module" do
+#  code "/usr/bin/passenger-install-apache2-module -a"
+#end
 
-template "/etc/apache2/mods-available/passenger.load" do
+cookbook_file "/etc/apache2/mods-available/passenger.load" do
   source "passenger.load"
 end
 
+bash "enable-stuff" do
+  code <<-EOH
+    a2enmod passenger
+    a2enmod rewrite
+    a2enmod ssl
+    a2ensite default-ssl
+  EOH
+end
+
+service "apache2" do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :restart ]
+end
