2688934205f715cc82b2054ab0b51e779a77f282
pj <me@paulgroudas.com>
2011-01-27 00:09:48 -0500

    Code cleanup, alphabetizing gems.

--- gitorious/recipes/gems.rb
+++ gitorious/recipes/gems.rb
@@ -1,57 +1,57 @@
-gem_package "rmagick" do
-  version "2.13.1"
-end
 gem_package "chronic" do
   version "0.3.0"
-end
-gem_package "geoip" do
-  version "0.8.9"
 end
 gem_package "daemons" do
   version "1.1.0"
 end
-gem_package "hoe" do
-  version "2.8.0"
+gem_package "diff-lcs" do
+  version "1.1.2"
 end
 gem_package "echoe" do
   version "4.3.1"
 end
-gem_package "ruby-yadis" do
-  version "0.3.4"
+gem_package "geoip" do
+  version "0.8.9"
 end
-gem_package "ruby-openid" do
-  version "2.1.8"
-end
-gem_package "mime-types" do
-  version "1.16"
-end
-gem_package "diff-lcs" do
-  version "1.1.2"
+gem_package "hoe" do
+  version "2.8.0"
 end
 gem_package "json" do
   version "1.4.6"
 end
-gem_package "rack" do
-  version "1.0.1"
-end
-gem_package "ruby-hmac" do
-  version "0.4.0"
-end
-gem_package "rake" do
-  version "0.8.7"
-end
-gem_package "stompserver" do
-  version "0.9.9"
+gem_package "mime-types" do
+  version "1.16"
 end
 gem_package "passenger" do
   version "3.0.2"
 end
+gem_package "rack" do
+  version "1.0.1"
+end
 gem_package "rails" do
   version "2.3.10"
+end
+gem_package "rake" do
+  version "0.8.7"
 end
 gem_package "rdiscount" do
   version "1.3.1.1"
 end
+gem_package "rmagick" do
+  version "2.13.1"
+end
+gem_package "ruby-hmac" do
+  version "0.4.0"
+end
+gem_package "ruby-openid" do
+  version "2.1.8"
+end
+gem_package "ruby-yadis" do
+  version "0.3.4"
+end
 gem_package "stomp" do
   version "1.1"
+end
+gem_package "stompserver" do
+  version "0.9.9"
 end
